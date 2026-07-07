3be691d1053d770a1b6aff2520fbb22500e5c4ab
Pablo Pamies Pérez <pablopamiesperez@gmail.com>
2026-07-07 08:49:25 +0000

    developed report_profit_by_customer and report_profit_by_product

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,28 @@
+{{ config(materialized='table') }}
+
+select 
+    o.orderid,
+    o.orderdate,
+    o.shipdate,
+    o.shipmode,
+    o.ordersellingprice - o.ordercostprice as orderprofit,
+    o.ordercostprice,
+    o.ordersellingprice,
+    c.customerid,
+    c.customername,
+    c.segment,
+    c.country,
+    p.productid,
+    p.productname,
+    p.category,
+    p.subcategory,
+from    
+    {{ ref ('raw_order') }} as o
+left join 
+    {{ ref('raw_customer') }} as c
+on 
+    o.customerid=c.customerid
+left join
+     {{ ref('raw_product' )}} as p
+on 
+    o.productid=p.productid
